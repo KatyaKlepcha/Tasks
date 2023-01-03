92917a05d2c8989c5055d0c457464c9200275547
KatyaKlepcha <solianchik@yandex.ru>
2023-01-03 15:31:31 +0300

    hw-8: sort by name, filter by age

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,34 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
+            let copyState = [...state]
+            return copyState.sort(function (a, b) {
+                if (action.payload === 'up') {
+                    if (a.name > b.name) {
+                        return 1
+                    }
+                    if (a.name < b.name) {
+                        return -1
+                    }
+                }
+                if (action.payload === 'down') {
+                    if (a.name > b.name) {
+                        return -1
+                    }
+                    if (a.name < b.name) {
+                        return 1
+                    }
+                }
 
-            return state // need to fix
+                return 0
+            }) // need to fix
         }
         case 'check': {
 
-            return state // need to fix
+            return state.filter(el => el.age >= action.payload) // need to fix
         }
         default:
             return state
